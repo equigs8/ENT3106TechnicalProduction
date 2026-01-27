b6c4c4bbfe9b400c40fe128d3160328e671ef3cc
equigs8 <equigsrus@gmail.com>
2026-01-27 16:32:35 -0500

    Week1 Progress

---
+++ Assets/Week1/Scripts/Week1PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Week1PlayerController : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public bool facingRight = false;
+
+    private SpriteRenderer leftArm;
+    private SpriteRenderer rightArm;
+    private SpriteRenderer leftLeg;
+    private SpriteRenderer rightLeg;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
