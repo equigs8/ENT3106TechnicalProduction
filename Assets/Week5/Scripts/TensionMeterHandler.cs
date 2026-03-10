38ce1e153dccfaa30b475419ada5d8d7cf56f818
August <goosey9999@gmail.com>
2026-03-09 22:56:19 -0400

    Added Tension Meter

--- Assets/Week5/Scripts/TensionMeterHandler.cs
+++ Assets/Week5/Scripts/TensionMeterHandler.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System; // Required when Using UI elements.
 
 public class TensionMeterHandler : MonoBehaviour
 {
+public Slider tensionMeter;
+
+public float speed;
+public bool meterStarted;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        tensionMeter.value = 0;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(meterStarted){
+           tensionMeter.value += speed;
+        }
         
     }
+
+    internal float GetTensionMeterValue(){
+        return tensionMeter.value;
+    }
+
 }
